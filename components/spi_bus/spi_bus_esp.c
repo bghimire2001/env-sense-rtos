7db3ea93416424efc169755f4fe48e7bda5e7401
Bipin Ghimire <bghimire2001@outlook.com>
2026-08-04 22:59:19 -0400

    more work on the bus abstraction layer

--- components/spi_bus/spi_bus_esp.c
+++ components/spi_bus/spi_bus_esp.c
@@ -1,13 +1,45 @@
 #include "spi_bus.h"
 #include "driver/spi_master.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+#include "esp_log.h"
+#include "string.h"
 
+#define SPI_CS1 10
+#define SPI_BUS_MOSI_GPIO 11
 #define SPI_BUS_SCLK 12
 #define SPI_BUS_MISO_GPIO 13
-#define SPI_BUS_MOSI_GPIO 11
-#define SPI_CS1 10
+
+#define SPI_BUS_MAX_DEVICES 4
+
+
 #define SPI_BUS_CLK_FRQ_HZ 4000000
 
+static SemaphoreHandle_t bus_lock = NULL;
+static bool initialized = false;
 
+static struct {
+    spi_device_handle_t handle;
+    uint8_t cs;
+    bool used;
+} dev_table [SPI_BUS_MAX_DEVICES];
+
+/* Error Transalation */
+
+static spi_err_t esp_to_bus_err(esp_err_t e)
+{
+    switch (e) {
+        case ESP_OK:                return BUS_OK;
+        case ESP_ERR_TIMEOUT:       return BUS_ERR_TIMEOUT;
+        case ESP_ERR_INVALID_ARG:   return BUS_ERR_INVALID;
+        case ESP_ERR_NOT_FOUND:     return BUS_ERR_NACK;
+        default:                    return BUS_ERR_IO;
+    }
+}
+
+/*
+Initialize the 
+*/
 spi_err_t spi_bus_init(void){
     return BUS_ERR_INVALID;
 }
